d07f9f9829ea95cdb2c4ef7e96f5556096e9061c
gudebin <gudebin@outlook.com>
2023-01-26 20:34:22 +0800

    update

--- cn/edu/example/HelloWorld.java
+++ cn/edu/example/HelloWorld.java
@@ -1,7 +1,20 @@
 package cn.edu.example;
 
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+//        Scanner scanner = new Scanner(System.in);
+        // start coding here
+        Scanner first = new Scanner(System.in);
+        String java = first.next();
+        Scanner second = new Scanner(System.in);
+        String programming = second.next();
+        Scanner third = new Scanner(System.in);
+        String language = third.next();
+        System.out.println(language);
+        System.out.println(programming);
+        System.out.println(java);
     }
 }
